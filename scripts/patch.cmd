86fd7f3170f098209cc6b2ed9dcd987b0b376cf2
Hibiki Tachibana <57486057+Hibbiki@users.noreply.github.com>
2021-09-21 22:27:20 +0200

    Apply patched as last step

--- scripts/patch.cmd
+++ scripts/patch.cmd
@@ -3,11 +3,11 @@
 IF NOT DEFINED IN_CHROMIUM_BUILDER (goto :EOF)
 cd %CHROMIUM_DIR%\depot_tools
 
-cd src
-FOR %%f IN (%CHROMIUM_DIR%\patch\*.patch) DO git am --signoff < %%f
-cd ..
-
 @rem vpython is used for autoninja on standalone but httplib2 is only present for python
 cd src\chrome
 cmd /c "vpython -m pip install httplib2"
 cd ..\..
+
+cd src
+FOR %%f IN (%CHROMIUM_DIR%\patch\*.patch) DO git am --reject --signoff < %%f
+cd ..
