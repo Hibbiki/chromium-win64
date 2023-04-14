86eb586a32a26881fd59f8c8994eac4c1e7d0280
Hibiki Tachibana <57486057+Hibbiki@users.noreply.github.com>
2023-04-14 20:31:01 +0200

    Update scripts/stable.sh to allow for manual version fallback

--- scripts/stable.sh
+++ scripts/stable.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 VERSIONS=$(curl -s 'https://versionhistory.googleapis.com/v1/chrome/platforms/win64/channels/stable/versions/all/releases?filter=fraction%3E0.9,endtime=none')
-RELEASES=$(curl -s 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows')
-CUR_VERSION=$(echo $VERSIONS | jq -r '.releases[0].version')
-CUR_BRANCH_POS=$(echo $RELEASES | jq -r ".[] | select(.version==\"$CUR_VERSION\").chromium_main_branch_position")
+CUR_VERSION=$(echo $VERSIONS | jq -e -r '.releases[0].version')
+if [ $? -ne 0 ]; then
+	read -p "Unable to determine current stable version, provide manually: " CUR_VERSION
+fi
 
-echo v$CUR_VERSION-r$CUR_BRANCH_POS > ../out/revision
+RELEASES=$(curl -s 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows')
+CUR_BRANCH_POS=$(echo $RELEASES | jq -e -r ".[] | select(.version==\"$CUR_VERSION\").chromium_main_branch_position")
+if [ $? -ne 0 ]; then
+	read -p "Unable to resolve version to branch position, provide manually: " CUR_BRANCH_POS
+fi
+
+echo v$CUR_VERSION-r$CUR_BRANCH_POS | tee ../out/revision >&2
 echo $CUR_VERSION
