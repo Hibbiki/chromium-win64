95464249838f0e9757f98c695e92a1258ba8c44b
Hibiki Tachibana <57486057+Hibbiki@users.noreply.github.com>
2023-09-05 23:09:15 +0200

    Enable NINJA_SUMMARIZE_BUILD

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -9,7 +9,7 @@ cmd /c "autoninja -C out/stable-sync-x64 mini_installer.exe"
 cmd /c "gn gen out\stable-nosync-x64"
 cmd /c "autoninja -C out/stable-nosync-x64 mini_installer.exe"
 
+cmd /c "gn clean out\stable-undefined-noarch"
 cmd /c "gn gen out\stable-undefined-noarch"
-cmd /c "autoninja -C out/stable-undefined-noarch -t clean"
 cmd /c "autoninja -C out/stable-undefined-noarch generate_version_info policy_templates pack_policy_templates"
 cd ..
