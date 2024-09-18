a30e9d880afcb0dcdb92d46075c958a611590e6b
Hibiki Tachibana <57486057+Hibbiki@users.noreply.github.com>
2024-09-18 18:40:41 +0200

    Update provision.cmd to account for .gclient already existing

--- scripts/provision.cmd
+++ scripts/provision.cmd
@@ -3,4 +3,4 @@
 IF NOT DEFINED IN_CHROMIUM_BUILDER (goto :EOF)
 cd %CHROMIUM_DIR%\depot_tools
 
-gclient && fetch chromium && xcopy /f /s /e /i ..\internal src\google_apis\internal
+gclient sync && xcopy /f /s /e /i ..\internal src\google_apis\internal
